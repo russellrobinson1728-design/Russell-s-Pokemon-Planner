0f97242a33eaf1f4f9f5ebfe4399e10ecc5e2e1c
russellrobinson1728-design <russellrobinson1728@gmail.com>
2026-08-31 18:45:03 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='league-os-v8';
+const CACHE='league-os-v9';
 const CORE=['./','./index.html','./manifest.json','./sw.js','./assets/russ-trainer.png','./assets/dj-trainer.png','./assets/coat-of-arms.svg','./assets/friend-guy.svg','./assets/friend-girl.svg','./assets/placeholder-pokemon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
