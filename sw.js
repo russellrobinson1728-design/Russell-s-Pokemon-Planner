da6ae1c0dc1a62ab6efa75283836e0e116fd8429
russellrobinson1728-design <russellrobinson1728@gmail.com>
2026-08-31 18:39:05 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='league-os-v7';
+const CACHE='league-os-v8';
 const CORE=['./','./index.html','./manifest.json','./sw.js','./assets/russ-trainer.png','./assets/dj-trainer.png','./assets/coat-of-arms.svg','./assets/friend-guy.svg','./assets/friend-girl.svg','./assets/placeholder-pokemon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>cached)))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>cached))) });
